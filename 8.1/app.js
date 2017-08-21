8a576ed6cbf2dd3964020fa1a08a392c6477f96d
DaeJin Jeong <devwithu@naver.com>
2017-08-21 15:33:25 +0900

    exam

---
+++ 8.1/app.js
@@ -0,0 +1,15 @@
+
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+app.use(function(request, response) {
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.end('<h1>Hello express</h1>');
+});
+
+
+http.createServer(app).listen(52273, function() {
+	console.log('Server running at http://127.0.0.1:52273');
+});
